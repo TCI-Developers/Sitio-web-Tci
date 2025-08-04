e8e632bc1ebcf84257f04b4e59051deee8f34ffb
AlexisTci <ing15@tciconsultoria.com>
2025-08-04 15:24:42 -0600

    feat: se agrego la nueva forma de obtener los datos al recibir gtml

--- src/pages/home-ui/BlogCard.tsx
+++ src/pages/home-ui/BlogCard.tsx
@@ -6,7 +6,22 @@ interface Propiedades {
     fondo?:string;
     id?:string;
   }
-const BlogCard = ({titulo,descripcion,fondo}:Propiedades) => {
+const BlogCard = ({titulo,descripcion,fondo,id}:Propiedades) => {
+
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = descripcion;
+
+  const paragraphs = tempDiv.querySelectorAll('p');
+
+  let firstRealText = '';
+  for (const p of paragraphs) {
+    const text = p.textContent?.trim();
+    if (text) {
+      firstRealText = text;
+      break;
+    }
+  }
+  
   return (
     <section
       className="flex items-end rounded-3xl overflow-hidden w-full h-full"
@@ -18,9 +33,9 @@ const BlogCard = ({titulo,descripcion,fondo}:Propiedades) => {
     >
       <div className="flex flex-col justify-end w-full p-4 sm:p-5 md:p-6 lg:p-8 bg-gradient-to-t from-black/60 via-black/30 to-transparent">
         <h3 className="text-white text-2xl font-bold font-bebas leading-none">
-          {titulo}
+          {titulo}  #{id}
         </h3>
-        <TextoCortadoConLink texto={descripcion} limite={250} urlDetalle={`/`} />
+        <TextoCortadoConLink texto={firstRealText} limite={250} urlDetalle={`/`} />
       </div>
     </section>
 
